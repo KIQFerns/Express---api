7c17766e3f8db10f5cc5ba5c25c590719a11f693
KIQFerns <52255280+KIQFerns@users.noreply.github.com>
2024-03-06 16:23:41 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -5,6 +5,6 @@ CREATE DATABASE desafio;
 CREATE TABLE clientes (
     id_cliente serial PRIMARY KEY,
     nome VARCHAR (50),
-    email VARCHAR (20),
-    telefone VARCHAR (10)
+    email VARCHAR (30),
+    telefone VARCHAR (30)
 );
