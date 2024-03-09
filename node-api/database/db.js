5e287ec9b9f674461aba30223bb1e489240817c1
KIQFerns <52255280+KIQFerns@users.noreply.github.com>
2024-03-08 21:32:56 -0300

    .env

--- node-api/database/db.js
+++ node-api/database/db.js
@@ -1,11 +1,12 @@
+const env = require("dotenv").config();
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  user: 'kiq',
-  host: 'localhost',
-  database: 'desafio',
-  password: 'Cas2cafor@',
-  port: 5432,
+  user: env.parsed.DATABASE_USER,
+  host: env.parsed.DATABASE_HOST,
+  database: env.parsed.DATABASE_DB,
+  password: env.parsed.DATABASE_PASSWORD,
+  port: env.parsed.DATABASE_PORT,
 })
 
 module.exports = pool;
